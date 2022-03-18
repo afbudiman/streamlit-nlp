62351b7dfc3ef83daec385c153da0f32837a2a04
Arif Budiman <52444340+afbudiman@users.noreply.github.com>
2022-03-18 21:09:15 +0700

    add transformer model

--- sentiment.py
+++ sentiment.py
@@ -1,10 +1,20 @@
-from cProfile import label
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 import streamlit as st
 
 def main():
     txt = st.text_area(label='Put your review here')
-    st.write('Sentiment:',txt)
-        
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    model_name = "aychang/roberta-base-imdb"
+
+    nlp = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+    results = nlp(txt)
+
+    st.write('Sentiment: ', results)
+    
 
 if __name__ == "__main__":
     st.set_page_config(page_title="Sentiment Analysis")
