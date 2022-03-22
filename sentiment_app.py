9981a2c0d27107e1f9af759b7624915e9e95dd66
Arif Budiman <52444340+afbudiman@users.noreply.github.com>
2022-03-22 14:46:40 +0700

    edit streamlit button

---
+++ sentiment_app.py
@@ -0,0 +1,21 @@
+from sentiment_model import text_cleaning
+import pickle
+import streamlit as st
+
+def main():
+    txt = st.text_area(label='Put your review here')
+
+    if st.button('Predict'):
+        text = text_cleaning(txt)
+        model = pickle.load(open('sentiment_model.pkl', 'rb'))
+        prediction = model.predict([text])
+        output = int(prediction[0])
+        probas = model.predict_proba([text])
+        output_probability = "{:.2f}".format(float(probas[:, output]))
+        sentiments = {0: "NEGATIVE", 1: "POSITIVE"}
+        st.write(f"It's {sentiments[output]} sentiment with {output_probability} probability")
+    
+if __name__ == "__main__":
+    st.set_page_config(page_title="Sentiment Analysis")
+    st.title("Sentiment Analysis")
+    main()
