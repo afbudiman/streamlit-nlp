becd0708eb441e3199ccb04fd59474f008703f03
Arif Budiman <52444340+afbudiman@users.noreply.github.com>
2022-04-05 13:44:36 +0700

    back to the beginning

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,47 +1,47 @@
-# import pandas as pd
-# import re
-# import nltk
-# from nltk.stem import WordNetLemmatizer 
-# from sklearn.pipeline import Pipeline
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# import pickle
+import pandas as pd
+import re
+import nltk
+from nltk.stem import WordNetLemmatizer 
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
 
-# nltk.download('omw-1.4')
-# nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('wordnet')
 
-# df = pd.read_csv('movie_data.csv', encoding='utf-8')
-# df = df.rename(columns={"0": "review", "1": "sentiment"})
+df = pd.read_csv('movie_data.csv', encoding='utf-8')
+df = df.rename(columns={"0": "review", "1": "sentiment"})
 
-# def text_cleaning(text, lemmatize_words=True):
-#     text = re.sub('<[^>]*>', '', text)
-#     emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
-#     text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', ''))
+def text_cleaning(text, lemmatize_words=True):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', ''))
     
-#     if lemmatize_words:
-#         text = text.split()
-#         lemmatizer = WordNetLemmatizer() 
-#         lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
-#         text = " ".join(lemmatized_words)
+    if lemmatize_words:
+        text = text.split()
+        lemmatizer = WordNetLemmatizer() 
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
+        text = " ".join(lemmatized_words)
 
-#     return text
+    return text
 
-# df["review"] = df["review"].apply(text_cleaning)
+df["review"] = df["review"].apply(text_cleaning)
 
-# X_train = df.loc[:25000, 'review'].values
-# y_train = df.loc[:25000, 'sentiment'].values
-# X_test = df.loc[25000:, 'review'].values
-# y_test = df.loc[25000:, 'sentiment'].values
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
 
-# tfidf = TfidfVectorizer(strip_accents=None,
-#                         lowercase=False,
-#                         preprocessor=None)
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
 
-# lr_tfidf = Pipeline([('vect', tfidf),
-#                      ('clf', LogisticRegression(solver='liblinear'))])
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(solver='liblinear'))])
 
-# lr_tfidf.fit(X_train, y_train)
+lr_tfidf.fit(X_train, y_train)
 
-# # print(f'Test Accuracy: {lr_tfidf.score(X_test, y_test):.3f}')
+# print(f'Test Accuracy: {lr_tfidf.score(X_test, y_test):.3f}')
 
-# # pickle.dump(lr_tfidf, open('sentiment_model.pkl','wb'))
+# pickle.dump(lr_tfidf, open('sentiment_model.pkl','wb'))
